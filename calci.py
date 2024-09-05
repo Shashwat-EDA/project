f7aeab3f3282a282acdc5092475b07cd49d3540a
Shashwat-EDA <shashwatmishra2906@gmail.com>
2024-09-05 14:23:29 +0530

    Update calci.py

--- calci.py
+++ calci.py
@@ -1,19 +1,14 @@
 num1=int(input("Enter the first number="))
 num2=int(input("Enter the second number="))
 ch=input("Enter your arthematic choice (+,-,/,*)=")
-if(ch='+')
-{
-print(num1+num2)
-}
-if(ch='*')
-{
-print(num1*num2)
-}
-if(ch='/')
-{
-print(num1/num2)
-}
-if(ch='-')
-{
-num1-num2
-}
+if ch == '+':
+  print(num1+num2)
+elif ch == '*':
+  print(num1*num2)
+elif ch == '/':
+  print(num1/num2)
+elif ch == '-':
+  print(num1-num2)
+else:
+  print("Invalid Operator")
+
